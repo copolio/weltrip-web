6e347a8ece6c5467d4b2d6b4e5b8ab6b6e9cec9d
Kijung <rlwjd1504@gmail.com>
2020-05-05 17:36:04 +0900

    simple profile

--- users/models.py
+++ users/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    image = models.ImageField(default='default.jpg', upload_to='profile_pics')
+    image = models.ImageField(default='default.png', upload_to='profile_pics')
 
     def __str__(self):
-        return f'{self.user.username} Profile'
+        return f'{self.user.username} 님의 프로필'
