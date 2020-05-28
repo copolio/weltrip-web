5c4067ac3017d442e77e6a99121d6efc110e6de2
cardiox95 <jslee811@gmail.com>
2020-05-28 23:02:55 +0900

    0528planner
    0528planner

--- actualPlanner/urls.py
+++ actualPlanner/urls.py
@@ -6,8 +6,10 @@ urlpatterns = [
     path('', views.index, name ='index'),
     path('plannerchoice/', views.plannerchoice, name ='plannerchoice'),
     path('createplan/', views.createplan, name ='createplan'),
+    path('makeplanid/', views.makeplanid, name ='makeplanid'),
     path('showplan/', views.showplan, name ='showplan'),
     path('cplan/', views.cplan, name='cplan'),
     path('reviseplan/', views.reviseplan, name='reviseplan'),
+    path('rankplan/', views.rankplan, name='rankplan'),
     
 ]
--- actualPlanner/views.py
+++ actualPlanner/views.py
@@ -14,15 +14,8 @@ def plannerchoice(request):
 def createplan(request):
     return render(request, 'planner/createplan.html')
 
-def showplan(request):
-    return render(request, 'planner/showplan.html')
-
-def reviseplan(request):
-    return render(request, 'planner/reviseplan.html')
-
-
-
-
+def makeplanid(request):
+    return render(request, 'planner/makeplanid.html')
 
 
 from search.search import *
@@ -48,7 +41,32 @@ def cplan(request):
     else:
         return render(request, 'planner/cp_createplan.html')
 
+def reviseplan(request):
+    if 'search-key' in request.POST and request.POST['search-key']:
+        siteKey = request.POST['search-key']
+        print(siteKey)
+        api_tmp = ApiInfo('1a%2FLc1roxNrXp8QeIitbwvJdfpUYIFTcrbii4inJk3m%2BVpFvZSWjHFmOfWiH9T7TMbv07j5sDnJ5yefVDqHXfA%3D%3D', 'http://api.visitkorea.or.kr/openapi/service/rest/KorWithService/')
 
+        results_tmp = searchByKeyword(siteKey, api_tmp)
+        print(results_tmp)
+        results_list = []
+        for elm in results_tmp:
+            elm_tmp = getInfos(elm)
+            dic_tmp = {'title':elm_tmp.get('title'), 'addr':elm_tmp.get('addr1'), 'contentId':elm_tmp.get('contentid')}
+            results_list.append(dic_tmp)
+        # output_list = checkInfos(result_list, 'title')
+        
+        print(results_list)
+        return render(request, 'planner/reviseplan.html', {'site_searched':results_list})
+
+    else:
+        return render(request, 'planner/reviseplan.html')
+
+def rankplan(request):
+    return render(request, 'planner/rankplan.html')
 
 def miniSearch(request):
     return render(request, 'planner/mini_search.html')
+
+def showplan(request):
+    return render(request, 'planner/showplan.html')
